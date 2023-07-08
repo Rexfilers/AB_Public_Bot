fa195b97752e93908c790dbaf1b739651b6f5101
Abmoviehouse <130202418+Abmoviehouse@users.noreply.github.com>
2023-07-09 00:13:37 +0545

    Update config.py

--- config.py
+++ config.py
@@ -25,7 +25,7 @@ class Config(object):
 This is Permanent Files Store Bot!
 Send me any file I will save it in my Database. Also works for channel. Add me to channel as Admin with Edit Permission, I will add Save Uploaded File in Channel & add Sharable Button Link.
 
-🤖 **My Name:** [Files Store Bot](https://t.me/{BOT_USERNAME})
+🤖 **My Name:** [AMIR BISTA](https://t.me/AB_BotZ_Update)
 
 📝 **Language:** [Python3](https://www.python.org)
 
